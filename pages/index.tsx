382dacc62f225b2a1488fd5f643b8a9eb1f8f610
Poyraz <poyrazavsever@hotmail.com>
2025-07-14 17:01:00 +0300

    feat: added last project section in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,51 @@ import Button from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="my-24">
-
-     
-
       <section className="max-w-7xl container mx-auto flex items-center justify-between relative">
-        <h1 className="text-3xl sm:text-5xl md:text-7xl font-advent-pro font-semibold max-w-lg">Huawei Student Developers OSTIM Teknik Üniversitesi</h1>
+        <h1 className="text-3xl sm:text-5xl md:text-7xl font-advent-pro font-semibold max-w-lg">
+          Huawei Student Developers OSTIM Teknik Üniversitesi
+        </h1>
 
         <div className="max-w-sm flex flex-col items-start gap-2">
           <p className="text-lg ">
-            Topluluğumuzun etkinlikleri kaçırmamak için hemen giriş yap, sen de bu etkinliklerde yer al! Hemen aşağıdaki butondan giriş yapabilirsin.
+            Topluluğumuzun etkinlikleri kaçırmamak için hemen giriş yap, sen de
+            bu etkinliklerde yer al! Hemen aşağıdaki butondan giriş
+            yapabilirsin.
           </p>
 
           <Button variant="primary">Giriş Yap</Button>
-
         </div>
 
         <div className="absolute -top-16 left-4 w-24 h-24 bg-primary rounded-full -z-10 blur-3xl" />
         <div className="absolute -bottom-24 right-16 w-24 h-24 bg-primary rounded-full -z-10 blur-3xl" />
-
       </section>
 
       <section className="flex items-center gap-3 my-24">
         <TeamSlider />
       </section>
 
+      <section className="max-w-7xl container mx-auto flex items-center justify-between my-24">
+        <div className="flex flex-col items-start gap-4">
+          <h2 className="text-3xl sm:text-5xl md:text-7xl font-advent-pro font-semibold max-w-lg">
+            Deprem Bilinci
+          </h2>
+          <p className="text-lg ">
+            Türkiye’de öğrencilerin, öğretmenlerin, ebeveynlerin ve çocukların
+            (4 kategori) deprem hakkında bilinçlenmesi için oluşturulmuş web
+            sitesi
+          </p>
+          <div className="flex items-center gap-4">
+            <Button variant="primary" href="/projects/deprem-bilinci">
+              Projeyi İncele
+            </Button>
+            <Button variant="outline" href="/projects/deprem-bilinci">
+              Bütün Projeleri Gör
+            </Button>
+          </div>
+        </div>
 
+        <img src="/images/mockap.png" alt="mockap for index page" />
+      </section>
     </div>
   );
 }
