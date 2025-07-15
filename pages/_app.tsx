b9757fe0e8b635a5719a2621e7000ca2249da89e
Poyraz <poyrazavsever@hotmail.com>
2025-07-15 15:24:28 +0300

    feat: added layout futures to app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+
+import { useRouter } from "next/router";
+
 import Layout from "@/components/layout/layout";
+import SettingsLayout from "@/components/layout/settingsLayout";
+import AdminLayout from "@/components/layout/adminLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const path = router.pathname;
+
+  let LayoutComponent = Layout;
+  if (path.startsWith("/settings")) {
+    LayoutComponent = SettingsLayout;
+  } else if (path.startsWith("/admin")) {
+    LayoutComponent = AdminLayout;
+  }
+
   return (
-    <Layout>
+    <LayoutComponent>
       <Component {...pageProps} />
-    </Layout>
+    </LayoutComponent>
   );
 }
