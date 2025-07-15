f7df23cb4f5d38ee3d887de8b49130b76c1a1f31
Poyraz <poyrazavsever@hotmail.com>
2025-07-15 22:53:27 +0300

    fix(style): deleted max-width atr in admin layout

--- components/layout/adminLayout.tsx
+++ components/layout/adminLayout.tsx
@@ -5,7 +5,7 @@ type AdminLayoutProps = React.PropsWithChildren<{}>;
 
 const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
   return (
-    <div className="flex gap-8 w-full max-w-7xl mx-auto py-12">
+    <div className="flex gap-8 w-full px-16 py-12">
       <AdminSidebar />
       <main className="flex-1 min-w-0">{children}</main>
     </div>
