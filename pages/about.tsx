65a486e1573c89215c2af38d0ff7e224aab129bc
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-16 15:25:48 +0300

    feat: done about page site

---
+++ pages/about.tsx
@@ -0,0 +1,76 @@
+
+import React, { useState } from 'react';
+import Button from '@/components/ui/button';
+import CoreTeamCard from '@/components/shared/coreTeamCard';
+import SelectBox from '@/components/ui/selectBox';
+
+const About = () => {
+    // SelectBox örnek verileri
+    const [department, setDepartment] = useState('all');
+    const [social, setSocial] = useState('all');
+    const departmentOptions = [
+        { label: 'Tümü', value: 'all' },
+        { label: 'Departman Başkanları', value: 'baskan' },
+        { label: 'Yazılım', value: 'yazilim' },
+        { label: 'Tasarım', value: 'tasarim' },
+    ];
+    const socialOptions = [
+        { label: 'Tümü', value: 'all' },
+        { label: 'Sosyal Medya Departmanı', value: 'sosyal' },
+        { label: 'Etkinlik', value: 'etkinlik' },
+    ];
+
+    return (
+        <section className='my-24 max-w-7xl container mx-auto flex flex-col items-start gap-12'>
+            <section className="max-w-7xl container mx-auto flex flex-col md:flex-row items-center justify-between relative gap-8 md:gap-0">
+                <h1 className="text-3xl sm:text-5xl md:text-7xl font-advent-pro font-semibold">
+                    Biz Kimiz, Ne Yapıyoruz?
+                </h1>
+
+                <div className="max-w-2xl flex flex-col items-start gap-2">
+                    <p className="text-2xl">
+                        Biz OSTİM Teknik Üniversitesinde kurulmuş olan bir topluluğuz. Çeşitli etkinlikler ve projeler yapıyoruz. Sende üye olmak ister misin?
+                    </p>
+
+                    <Button variant="primary">Hemen Üye Ol</Button>
+                </div>
+
+                <div className="absolute -top-16 left-4 w-24 h-24 bg-primary rounded-full -z-10 blur-3xl" />
+                <div className="absolute -bottom-24 right-16 w-24 h-24 bg-primary rounded-full -z-10 blur-3xl" />
+            </section>
+            <section className="w-full flex flex-col gap-8 my-36">
+                <div className="flex flex-col md:flex-row items-center justify-between w-full gap-4">
+                    <h2 className="text-3xl sm:text-4xl font-advent-pro font-bold text-white">Ekibimizle Tanış!</h2>
+                    <div className="flex gap-4">
+                        <SelectBox options={departmentOptions} value={department} onChange={setDepartment} />
+                        <SelectBox options={socialOptions} value={social} onChange={setSocial} />
+                    </div>
+                </div>
+                <div className="w-full flex flex-wrap justify-between gap-8">
+                    {/* Örnek 3 kart */}
+                    <CoreTeamCard />
+                    <CoreTeamCard />
+                    <CoreTeamCard />
+                    <CoreTeamCard />
+                </div>
+            </section>
+            <section className="max-w-7xl container mx-auto flex flex-col md:flex-row items-center justify-between relative gap-8 md:gap-0 my-24">
+                <div className="max-w-2xl flex flex-col items-start gap-2">
+                    <p className="text-lg">
+                        HUAWEI Student Developers (HSD), öncü teknolojilere tutku duyan üniversite öğrencileri için küresel bir programdır. Geliştirici olarak büyümek isteyen tüm lisans ve lisansüstü öğrenciler başvuruda bulunabilir. HSD, öğrencilere zenginleştirici eğitim kursları ve etkinlikler aracılığıyla dinamik bir akranlar arası öğrenme ortamında bilgi birikimlerini genişletme fırsatı sunar.
+                    </p>
+
+                    <Button as='link' href='https://developer.huawei.com/consumer/en/programs/hsd/' variant="primary">Detaylar</Button>
+                </div>
+
+                <div className="absolute -bottom-24 right-1/2 w-64 h-64 bg-primary/20 rounded-full -z-20 blur-3xl" />
+
+                <h1 className="text-right text-3xl sm:text-5xl md:text-7xl font-advent-pro font-semibold max-w-lg">
+                    HSD Nedir, Biz Neyin Parçasıyız?
+                </h1>
+            </section>
+        </section>
+    );
+}
+
+export default About
