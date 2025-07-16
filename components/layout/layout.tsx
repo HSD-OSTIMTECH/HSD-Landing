a78f2cc2b5380f291f6df5583ca58dfdb8a98738
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-16 16:00:42 +0300

    feat: added modals to layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,13 +4,17 @@ import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
 
+// Modals
+import LoginModal from '../modals/loginModal';
+import RegisterModal from '../modals/registerModal';
+
 
 type LayoutProps = React.PropsWithChildren<{}>;
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
     <div className='flex flex-col min-h-screen'>
-        <Navbar isLogin={true}/>
+        <Navbar isLogin={false}/>
         
         <main className='flex-grow'>
             {children}
