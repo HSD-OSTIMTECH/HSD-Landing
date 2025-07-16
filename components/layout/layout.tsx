78ddc6d477fa99b620ecace984f2ac22cb59c99b
Poyraz <poyrazavsever@hotmail.com>
2025-07-16 18:58:52 +0300

    fix: fixed layout problem

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -31,7 +31,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         isLogin={false}
         onOpenLogin={() => setShowLogin(true)}
       />
-      <main className='flex-grow'>
+      <main className='flex-grow my-24'>
         {children}
       </main>
       <Footer />
