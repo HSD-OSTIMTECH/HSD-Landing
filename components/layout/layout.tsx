e6ad39f9ecda22cc41eab7c6ba7336dba1d53088
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-09-10 11:35:29 +0300

    refactor: remove unused modal logic and improve layout structure

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,48 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 // Componenst
 import Navbar from './navbar'
 import Footer from './footer'
 
-// Modals
-import LoginModal from '../modals/loginModal';
-import RegisterModal from '../modals/registerModal';
-
-
 type LayoutProps = React.PropsWithChildren<{}>;
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [showLogin, setShowLogin] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
-
-  // Modal geçiş fonksiyonları
-  const handleOpenRegister = () => {
-    setShowLogin(false);
-    setTimeout(() => setShowRegister(true), 200);
-  };
-  const handleOpenLogin = () => {
-    setShowRegister(false);
-    setTimeout(() => setShowLogin(true), 200);
-  };
 
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar
         isLogin={false}
-        onOpenLogin={() => setShowLogin(true)}
       />
-      <main className='flex-grow my-24'>
+      <main className='flex-grow my-24 z-0'>
         {children}
       </main>
       <Footer />
-
-      {/* Modaller burada */}
-      {showLogin && (
-        <LoginModal onClose={() => setShowLogin(false)} onOpenRegister={handleOpenRegister} />
-      )}
-      {showRegister && (
-        <RegisterModal onClose={() => setShowRegister(false)} onOpenLogin={handleOpenLogin} />
-      )}
     </div>
   )
 }
