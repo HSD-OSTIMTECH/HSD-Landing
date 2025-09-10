05dbf5b8fef0f7992a7bb89ff9ad45edaff905dc
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-09-10 15:24:02 +0300

    feat: add ApplyProject page with authentication component and user access prompts

---
+++ app/(site)/projects/apply/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Auth from '@/components/shared/auth'
+
+const ApplyProject = () => {
+  return (
+    <div>
+        <Auth />
+    </div>
+  )
+}
+
+export default ApplyProject
